d10af7a5db7a7e6ae3d95d3baa0914270ffa6532
Seth Opoku Akomeah <82619281+oaseth@users.noreply.github.com>
2023-01-09 00:43:04 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $server = 'localhost';
 $username = 'postgres';
-$password = 'Stoic358Pg';
+$password = '*********';
 $db_name = 'spatialdata';
 
 $dbconnection = pg_connect("host=$server port=5432 dbname=$db_name user=$username password=$password");
